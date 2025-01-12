732fe9c154e476b8605a3fa9541d328c67f0b0d9
cyphics <cyphics@protonmail.com>
2025-01-12 22:29:14 +0100

    basic structure

---
+++ build/CMakeFiles/algorithms.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algorithms.dir/link.d"
+  "CMakeFiles/algorithms.dir/main.c.o"
+  "CMakeFiles/algorithms.dir/main.c.o.d"
+  "algorithms"
+  "algorithms.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/algorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
